38d98f0c54881dd08f0ee87cc009a093613c65bc
Leftium <john@leftium.com>
2021-03-01 12:17:47 +0900

    Basic grid layout.

--- src/App.svelte
+++ src/App.svelte
@@ -18,35 +18,50 @@
 </script>
 
 <template lang="pug">
-  main
-    h1 Hello {name}!
-    p.
-      Visit the #[a(href='https://svelte.dev/tutorial') Svelte tutorial]
-      to learn how to build Svelte apps.
+  div.grid-wrapper
+    header
+      div HEADER
+    aside
+      div ASIDE
+    main
+      h1 Hello {name}!
+      p.
+        Visit the #[a(href='https://svelte.dev/tutorial') Svelte tutorial]
+        to learn how to build Svelte apps.
 
-    // How reactive buttons are done in Svelte + Pug.
-    // Note CoffeeScript cannot be used here.
-    button(on:click='{handleClick}') Clicked {count} {count === 1 ? 'time' : 'times'}
+      // How reactive buttons are done in Svelte + Pug.
+      // Note CoffeeScript cannot be used here.
+      button(on:click='{handleClick}') Clicked {count} {count === 1 ? 'time' : 'times'}
+    footer
+      div FOOTER
 </template>
 
 <style>
+  .grid-wrapper {
+    height: 100%;
+    display: grid;
+    grid-template-columns: 300px 1fr;
+    grid-template-rows: min-content auto min-content;
+    background-color: #ffafb0;
+  }
+  aside {
+    grid-column: 1/2;
+    grid-row: 2/3;
+    background-color: #004c6d;
+  }
+  header {
+    grid-column: 1/3;
+    grid-row: 1/2;
+    background-color: #4c7c9b;
+  }
+  footer {
+    grid-column: 1/3;
+    grid-row: 3/4;
+    background-color: #86b0cc;
+  }
   main {
-    text-align: center;
-    padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
-  }
-
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
-    }
+    grid-column: 2/3;
+    grid-row: 2/3;
+    background-color: #c1e7ff;
   }
 </style>
